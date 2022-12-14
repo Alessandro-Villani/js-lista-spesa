2c0dc703c974417b8a10e7b860bd7d6c37477870
Alessandro-Villani <alessvill@libero.it>
2022-12-14 15:37:55 +0100

    added logic - end

--- js/script.js
+++ js/script.js
@@ -3,3 +3,30 @@
 Data una lista della spesa (inventatela), stampare in pagina gli elementi della lista individualmente con un ciclo while.
 
 ----------------------------------------------------------------------------------------*/
+
+//1. Create product Array
+
+const products = ['pasta', 'uova', 'latte', 'vino', 'salsa'];
+
+//2. Pick Output element
+
+const outputShoppingList = document.getElementById('shopping-list');
+
+//3. Initializing list items variable
+
+let listItems = '';
+
+//4. Creating index for loop
+
+let i = 0;
+
+//5. While loop to create list items
+
+while(i < products.length) {
+    listItems += `<li>${products[i]}</li>`;
+    i++
+}
+
+//6. Print list items in list
+
+outputShoppingList.innerHTML = listItems;
