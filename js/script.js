d8de5fbf65a27e27fdc87a0079bacad053be2dd6
Alessandro-Villani <alessvill@libero.it>
2022-12-14 15:25:34 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/*-------------------------------------TRACCIA--------------------------------------------
+
+Data una lista della spesa (inventatela), stampare in pagina gli elementi della lista individualmente con un ciclo while.
+
+----------------------------------------------------------------------------------------*/
